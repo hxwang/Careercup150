6281411986bcc3dbf12d81bdcc859cf2c85930b0
Huangxin <maggiewangsjtu@gmail.com>
2015-01-08 22:56:30 -0500

    Create 3.4.java

---
+++ code/3.4.java
@@ -0,0 +1,37 @@
+
+public class Tower{
+  private Stack<Integer> disks;
+  private int index;
+  public Tower(int i){
+    disks = new Stack<Integer>();
+    index = i;
+  }
+  
+  public int index(){
+    return index;
+  }
+  
+  public void add(int d){
+    if(!disk.isEmpty() && disk.peek()<=d)
+      System.out.println("Error placing disk" + d);
+    else disks.push(d);
+  }
+  
+  public void moveTopTo(Tower t){
+    int top = disks.top();
+    t.add(top);
+    System.out.println("Move disk" + top + "from" + index() + "to" + t.index());
+    
+  }
+  
+  public void moveDisks(int n, Tower destination, Tower buffer){
+    if(n>0){
+      //move n-1 disks from Tower 1 to Tower 2, using Tower 3
+      moveDisks(n-1, buffer, destination);
+      //move top from Tower 1 to Tower 3
+      moveTopTo(destination);
+      //move n-1 disks from Tower 2 to Towre 3, using Tower 1 as buffer
+      buffer.moveDisks(n-1, destination, this);
+    }
+  }
+}
