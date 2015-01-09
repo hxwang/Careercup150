6b467816b7a39fd9cbe5dfb8022649055097b53c
Huangxin <maggiewangsjtu@gmail.com>
2015-01-08 22:33:37 -0500

    Update 3.3.java

--- code/3.3.java
+++ code/3.3.java
@@ -25,3 +25,23 @@ public int pop(){
   if(last.size()==0) stacks.remove(stacks.size()-1);
   return v;
 }
+
+public int popAt(int index){
+  return leftShift(index, true);
+}
+
+public int leftShift(int index, boolean removeTop){
+  Stack stack = stacks.get(index);
+  int removed_item;
+  //remove top
+  if(removeTop) removed_item = stack.pop();
+  else removed_item = stack.removeBottom();
+  if(stack.isEmpty)
+    stacks.remove(index);
+  else if(stacks.size()>index+1){
+    //recursive shift to fill the empty slot
+    int v = leftShift(index+1, false);
+    stack.push(v);
+  }
+  return removed_item;
+}
