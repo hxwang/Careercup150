3df8bb9890cf7ce56cd029cb9bb6fe59f2224913
Huangxin <maggiewangsjtu@gmail.com>
2015-01-09 10:47:42 -0500

    Update 3.6.java

--- code/3.6.java
+++ code/3.6.java
@@ -1,3 +1,18 @@
 
-//stack s1 contains unsorted element
-//stac
+//stack s contains unsorted elements
+//stack r used to contain sorted elements
+
+public static Stack<Integer> sort(Stack<Integer> s){
+  
+  Stack<Integer> r = new Stack<Integer>();
+  while(!s.isEmpty()){
+    int tmp = s.pop();
+    //push tmp to the right place in r
+    while(!r.isEmpty() && r.peek() > tmp){
+      s.push(r.pop());
+    }
+    r.push(top);
+  }
+  
+  return r;
+}
