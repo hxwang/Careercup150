b0a235206131a4e3ad07436719c38ea900108891
Huangxin <maggiewangsjtu@gmail.com>
2015-01-09 22:45:16 -0500

    Create 5.7.java

---
+++ code/5.7.java
@@ -0,0 +1,40 @@
+
+
+//by observer the rountine
+
+//if count(0)<= count(1), LSB(v) =0
+//else LSB(v) =1
+
+//in the proces: discard all the numbers where LSB_i(x) != LSB_i(v)??
+
+//run time complexity: O(n)
+
+public int findMissing(ArrayList<BitInteger> array){
+  //BitInteger.INTEGER_SIZE-1 corresponds to LSB
+  return findMissing(array, BitInteger.INTEGER_SIZE-1);
+}
+
+public int findMissing(ArrayList<BitInteger> input, int column){
+  if(column < 0) return 0;
+  
+  ArrayList<BitInteger> oneBits = new ArrayList<BitInteger>(input.size()/2);
+  ArrayList<BitInteger> zerosBits = new ArrayList<BitInteger>(input.size()/2);
+  
+  for(BitInteger t: input){
+    if(t.fetch(column) == 0)
+      zeroBits.add(t);
+    else onesBits.add(t);
+  }
+  
+  if(zerosBits.size()<=0 onesBits.size()){
+    int v = findMissing(zeroBits, column-1);
+    return (v<<1) | 0;
+  }
+  else{
+    int v = findMissing(oneBits, column-1);
+    return (v<<1)|1;
+  }
+}
+
+
+
