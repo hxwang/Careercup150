49e1901e952fb8e470a6d8c634b86b94ab93c03c
Huangxin <maggiewangsjtu@gmail.com>
2015-01-09 20:19:35 -0500

    Update 5.2.java

--- code/5.2.java
+++ code/5.2.java
@@ -24,3 +24,26 @@ public static String printBinary(double num){
   
   return binary.toString();
 }
+
+//Approach 2: compare with 0.5, 0.25 etc..
+
+public static String printBinary(double num){
+  if(num >=1 || num<=0) return "ERROR";
+  
+  StringBuilder sb = new StringBuilder();
+  double frac = 0.5;
+  while(num > 0){
+    if(sb.length()>=32) return "ERROR";
+    //compare and append
+    if(num >= frac){
+      sb.append("1");
+      num -= frac;
+    }
+    else{
+      sb.append("0");
+    }
+    //update fractor
+    frac /=2;
+  }
+  return sb.toString();
+}
