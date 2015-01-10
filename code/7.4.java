b083994e82a9d8abe4a789ca34fa7e95561e5995
Huangxin <maggiewangsjtu@gmail.com>
2015-01-10 17:05:01 -0500

    Update 7.4.java

--- code/7.4.java
+++ code/7.4.java
@@ -20,3 +20,43 @@ public static int negate(int a){
 public static int minus(int a, int b){
   return a + negate(b);
 }
+
+
+//Multiplication
+public static int multiply(int a, int b){
+  
+  //make the multiply more efficient
+  if(a<b) return mutiply(b,a);
+  
+  int sum = 0;
+  for(int i = Math.abs(b); i>0; i--)
+    sum += a;
+  
+  if(b<0) sum = negate(sum);
+  
+  return sum;
+}
+
+//division
+//adding b to itself, until exceed a
+public int divide(int a, int b) throws java.lang.ArithmeticException{
+  
+  if(b==0)
+    throws java.lang.ArithmeticException("ERROR");
+    
+  int absa = abs(a);
+  int absb = abs(b);
+  
+  int product = 0;
+  int x = 0;
+  while(product + absb <= absa){
+    product += absb;
+    x++;
+  }
+  
+  if( (a<0 && b<0) || (a>0 && b>0))
+    return x;
+  else return negate(x);
+}
+
+
