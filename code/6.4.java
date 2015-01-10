1cd3d701095fa9042fefb9666527e0f1fbe3ebe0
Huangxin <maggiewangsjtu@gmail.com>
2015-01-10 11:48:38 -0500

    Create 6.4.java

---
+++ code/6.4.java
@@ -0,0 +1,10 @@
+
+//let c denote the number of blue-eyed people
+
+//if c=1, then take 1 day, since we assume there is at least 1 blue-eyed people
+
+//if c=2; then on the first day, no one leave; which indicates there are more than 1 blue-eyed pepple, so in the second day, they leave
+
+//if c =3; then if no one leave on the second day, it means there are more than 2 people, thus on the 3rd day they leave
+
+//if c = k, then it takes k days for the blue eyed people to leave
