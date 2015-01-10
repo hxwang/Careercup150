002959a43a3f91d8e541d2b6bd4d6f77fedbd664
Huangxin <maggiewangsjtu@gmail.com>
2015-01-10 12:08:53 -0500

    Create 6.6.java

---
+++ code/6.6.java
@@ -0,0 +1,6 @@
+
+//the perfect square number will have odd number of factors
+
+//for e.g., 16 will have (1, 16) (2,8) (4,4), 4 will only be count once.
+
+//thus there are 10 perfect square numbers in (1,100)
