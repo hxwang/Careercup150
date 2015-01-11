7b419e36b9ff2b0ada8e1c9e018cec2117677117
Huangxin <maggiewangsjtu@gmail.com>
2015-01-10 20:45:30 -0500

    Update 8.1.java

--- code/8.1.java
+++ code/8.1.java
@@ -61,3 +61,36 @@ public class Hand<T extends Card>{
     cards.add(card);
   }
 }
+
+public class BlackJackhand extends Hand<BlackJackCard>{
+  
+  public int score(){
+    
+    ArrayList<Integer> scores = possibleScores();
+    int maxUnder = Integer.MIN_VALUE;
+    int minOver = Integer.MAX_VALUE;
+    for(int score: scores){
+      if(score >21 && score<minOver){
+        minOver = score;
+      }
+      else if(score <=21 && score> maxUnder)
+        maxUnder = score;
+    }
+    
+    return maxUnder == Integer.MIN_VALUE? minOver: maxUnder;
+  }
+  
+  private ArrayList<Integer> possibleScores() {...}
+  
+  public boolean busted () {return score() > 21;}
+  public boolean is21() {return score() == 21;}
+  public boolean isBlackJack() {...}
+}
+
+public class BlackJackCard extends Card{
+  public BlackJackCard(int c, Suit s){
+    super(c,s);
+  }
+  
+  
+}
