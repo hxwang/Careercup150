82c81edeec34bafe79df59020bb5aac417ca1ac3
Huangxin <maggiewangsjtu@gmail.com>
2015-01-10 20:48:16 -0500

    Update 8.1.java

--- code/8.1.java
+++ code/8.1.java
@@ -92,5 +92,30 @@ public class BlackJackCard extends Card{
     super(c,s);
   }
   
+  //redefine value
+  //this is the reason that we define Card class as abstract, as the value will only be meaningful with specific game
+  public int value(){
+    if(isAce()) return 1;
+    else if(faceValue >= 11 && faceValue <=12) return 10;
+    else return faceValue;
+  }
+  
+  public int minValue(){
+    if(isAce()) return 1;
+    else return value();
+  }
+  
+  public int maxValue(){
+    if(isAce()) return 11;
+    else return value;
+  }
+  
+  public boolean isAce(){
+    return faceValue == 1;
+  }
+  
+  public boolean isFacedCard(){
+    return faceValue >= 11 && faceValue <= 13;
+  }
   
 }
