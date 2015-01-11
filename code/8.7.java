b54800ed65d4dcc76bffde94cecd5846a67ad013
Huangxin <maggiewangsjtu@gmail.com>
2015-01-11 01:09:08 -0500

    Update 8.7.java

--- code/8.7.java
+++ code/8.7.java
@@ -37,3 +37,75 @@ public class User{
   
   public boolean isOnline() {return online;}
 }
+
+
+//////////////////////////////////////////////////// Reference
+
+//UserManager
+
+public class UserManager{
+  
+  private static UserManager instance;
+  
+  //map from a user id to a user
+  private HashMap<Integer, User> usersById;
+  
+  //maps from an account name to a user
+  private HashMap<String, User> usersByAccountName;
+  
+  //maps from the user id to an online user
+  private HashMap<Ineger, User> onlineUsers;
+  
+  public static UserManager getInstance(){
+    if(instance == null) instance = new UserManager();
+    return instance;
+  }
+  
+  public void adduser(User fromUser, Strong toAccountName) {}
+  public void approveAddRequest(AddRequest req) {}
+  public void rejectAddRequest(AddRequest req) {}
+  public void userSignedOn(String accountName) {}
+  public void userSignedOff(String accountName) {}
+  
+}
+
+
+//If A want to add B
+//A will call UserManager requestAdd
+//UserManager will call: A.sendAddRquest and B.receiveAddRequest
+
+///////////////////////////////////////////////////////////////////
+public class User{
+  private int id;
+  private UserStatus status = null;
+  
+  //mao from the other participant's user id to the chat
+  private HashMap<Integer, PrivateChat> privatechats;
+  
+  //map from the group chat id to the group chat
+  private ArrayList<GroupChat> groupchats;
+  
+  //map from the other person's user id to the add request
+  private HashMap<Integer, AddRequest> receivedAddRquests;
+  
+  //map from the other person's user id to the add request
+  private HashMap<Integer, AddRequest> sentAddRequests;
+  
+  private String accountName;
+  private String fullName;
+  
+  public User(int id, String accountName, String fullName) {...}
+  public boolean sendMessageToUser(User to, String content) {...}
+  public boolean sendMessageToGroupChat(int id, String content) {...}
+  public void setStatus(UserStatus status) {...}
+  public UserStatus getStatus() {...}
+  public boolean addContact(User user) {...}
+  public void receivedAddRequest(AddRequest req) {...}
+  public void sentAddRequest(AddRequest req) {...}
+  public void removeAddRequest(AddRequest req) {...}
+  public void requestAddUser(String accountName) {...}
+  public void addConversation(PrivateChat conversation) {...}
+  public void addConversation(GroupChat conversation) {...}
+  public int getId() {...}
+  
+}
