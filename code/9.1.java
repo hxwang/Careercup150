a825b9b62c0bcb40d1f7d49d98ca69c3271630b9
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 14:19:55 -0500

    Update 9.1.java

--- code/9.1.java
+++ code/9.1.java
@@ -9,7 +9,7 @@ int climbStair(int n){
   step[2] = step[0] + ste[1] + 1;
   if(n<=3) return step[n-1];
   for(int i=3; i<n; i++)
-    step[n] = step[n-3] + ste[n-2] + step[n-1];
+    step[n] = step[n-3] + step[n-2] + step[n-1];
   
   return step[n-1];
 }
