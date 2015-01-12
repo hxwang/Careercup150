51c7f73e517307db4d0bfdb9a29e1c868d748ed2
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 14:32:54 -0500

    Create 9.2.java

---
+++ code/9.2.java
@@ -0,0 +1,16 @@
+
+//////////// My implementation
+
+public int ways(int X, int Y, boolean[][] cango){
+  int[][] ways = new int[X+1][Y+1];
+  ways[1][1] = 1;
+
+  for(int i=1; i<=X; i++){
+    for(int j=1; j<=Y; j++){
+      if(i==1 && j==1) ways[1][1] = cango[0][0]==true? 1: 0;
+      else ways[i][j] = cango[i-1][j-1]==true ? Math.max(ways[i-1][j], ways[i][j-1]) : 0;
+      }
+  }
+  
+  return ways[X][Y];
+}
