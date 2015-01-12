90746a746d4a3eb310e54457542b1fa0db6c74c7
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 15:03:35 -0500

    Update 9.3.java

--- code/9.3.java
+++ code/9.3.java
@@ -8,9 +8,12 @@ public int magicIndex(int left, int right, int[] A){
   
   int mid = (left+right)/2;
   if(A[mid] == mid) return mid;
+  
+  //difference inverse
   if( (A[left] < left && A[mid]>mid) || (A[left]>left && A[mid]<mid))
     return magicIndex(left, mid-1, A);
   else if( (A[mid]<mid && A[right]>right) || (A[mid]>mid && A[right] < right))
     return magicIndex(mid+1, right, A);
+    
   else return -1;
 }
