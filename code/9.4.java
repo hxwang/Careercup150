1b37c2237b4b86ba405660a60d3ffd229b61aee7
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 15:17:25 -0500

    Create 9.4.java

---
+++ code/9.4.java
@@ -0,0 +1,20 @@
+
+ArrayList<ArrayList<Integer>> getSubset(ArrayList<Integer> set){
+  ArrayList<ArrayList<Integer>> rnt = new ArrayList<ArrayList<Integer>>();
+  ArrayLIst<Integer> tmp = new ArrayList<Integer>();
+  int start = 0;
+  dfs(rnt, tmp, set,start);
+}
+
+public void dfs(ArrayList<ArrayList<Integer>> rnt, ArrayList<Integer> tmp, ArrayList<Integer> set, int start){
+  if(start==set.size()){
+    rnt.add(new ArrayList<Integer>(tmp));
+    return;
+  }
+  
+  for(int i=start; i<set.size(); i++){
+    tmp.add(set.get(i));
+    dfs(rnt, tmp, set, i);
+    tmp.remove(set.get(i));
+  }
+}
