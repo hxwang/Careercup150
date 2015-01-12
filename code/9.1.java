933d8eae423cd5958b203d39881005b4c0471fc8
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 14:19:43 -0500

    Create 9.1.java

---
+++ code/9.1.java
@@ -0,0 +1,15 @@
+
+
+int[] steps = new int[n];
+
+int climbStair(int n){
+  //let step[i] denote the number of ways to climb to (i+1) step
+  step[0] = 1;
+  step[1] = 2;
+  step[2] = step[0] + ste[1] + 1;
+  if(n<=3) return step[n-1];
+  for(int i=3; i<n; i++)
+    step[n] = step[n-3] + ste[n-2] + step[n-1];
+  
+  return step[n-1];
+}
