76ecac8298e01d10992c47cc75f089c0cce79460
Huangxin <maggiewangsjtu@gmail.com>
2015-01-12 14:34:05 -0500

    Update 9.2.java

--- code/9.2.java
+++ code/9.2.java
@@ -8,7 +8,7 @@ public int ways(int X, int Y, boolean[][] cango){
   for(int i=1; i<=X; i++){
     for(int j=1; j<=Y; j++){
       if(i==1 && j==1) ways[1][1] = cango[0][0]==true? 1: 0;
-      else ways[i][j] = cango[i-1][j-1]==true ? Math.max(ways[i-1][j], ways[i][j-1]) : 0;
+      else ways[i][j] = cango[i-1][j-1]==true ? ways[i-1][j] + ways[i][j-1] : 0;
       }
   }
   
